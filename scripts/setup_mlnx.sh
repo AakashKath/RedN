13e4e732be5d578495604acc01368948aedfc93e
kath <ge86guy@mytum.de>
2022-12-05 20:16:44 +0100

    added -y flag

--- scripts/setup_mlnx.sh
+++ scripts/setup_mlnx.sh
@@ -4,6 +4,6 @@ cd mlnx
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.7-1.0.0.1/MLNX_OFED_LINUX-4.7-1.0.0.1-ubuntu18.04-x86_64.tgz
 tar -xvf MLNX_OFED_LINUX-4.7-1.0.0.1-ubuntu18.04-x86_64.tgz
 cd MLNX_OFED_LINUX-4.7-1.0.0.1-ubuntu18.04-x86_64
-sudo apt install createrepo
+sudo apt install -y createrepo
 yes | sudo ./mlnxofedinstall --add-kernel-support --kernel 4.13.16-041316-generic --without-fw-update --force-dkms
 sudo /etc/init.d/openibd restart
